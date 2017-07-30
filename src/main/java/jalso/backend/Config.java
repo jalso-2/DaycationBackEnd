2c3117612c299292ec53a32bb3c56aece6d8a83f
charlifrank <jayfrank0528@gmail.com>
2017-07-29 19:53:51 -0500

    (maint) Test key

--- src/main/java/jalso/backend/Config.java
+++ src/main/java/jalso/backend/Config.java
@@ -1,7 +1,7 @@
 package jalso.backend;
 
 public class Config {
-  private String _googleKey = "";
+  private String _googleKey = "key goes here";
 
   public String getKey() {
     return _googleKey;
